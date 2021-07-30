5df5b9539c948a400915444c11865b21f683c63c
Junior Santos <jnrplay@hotmail.com>
2021-07-30 14:18:37 -0300

    chore: config of commitlint husky prettier eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const foo = "bla";
   return (
     <div className="App">
       <header className="App-header">
