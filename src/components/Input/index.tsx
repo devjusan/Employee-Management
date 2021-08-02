13689346e0b0882803ee2cbb0dca2d8e19a70459
Junior Santos <jnrplay@hotmail.com>
2021-08-01 21:49:03 -0300

    feat: added system of erros in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,28 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { InputHTMLAttributes } from "react";
 import * as S from "./styles";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  name: string;
   value: string;
-  onChange: (arg: never) => never;
+  inputError: boolean;
+  name: string;
+  onChange: (arg: never) => any;
 }
 
-const Input: React.FC<InputProps> = ({ name, onChange, value, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  onChange,
+  name,
+  value,
+  inputError,
+  ...rest
+}) => {
+  const [error, setError] = React.useState(inputError);
   const [inputValue, setInputValue] = React.useState(value);
 
   React.useEffect(() => {
     if (inputValue === null) setInputValue("");
   }, [inputValue]);
 
+  React.useEffect(() => {
+    setError(inputError);
+  }, [inputError]);
+
   return (
     <S.Container>
       <S.Input
-        type="text"
         placeholder={name}
+        type="text"
         {...rest}
         onChange={onChange}
         value={value}
       />
+      <S.Error>{error ? "Campo obrigatório" : ""}</S.Error>
     </S.Container>
   );
 };
